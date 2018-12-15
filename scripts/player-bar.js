a4d595c0f1557f75e4c00db53a3e22935ebe89ab
siuan <x@siuan.com>
2018-12-15 17:37:09 -0500

    checkpoint blocjams 4: completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -8,7 +8,6 @@
    $('button#next').on('click', function() {
           if (player.playState !== 'playing') { return; }
 
-
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
      const nextSongIndex = currentSongIndex + 1;
      if (nextSongIndex >= album.songs.length) { return; }
@@ -17,9 +16,9 @@
      player.playPause(nextSong);
    });
 
+
    $('button#previous').on('click', function() {
           if (player.playState !== 'playing') { return; }
-
 
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
      const nextSongIndex = currentSongIndex - 1;
@@ -28,4 +27,17 @@
      const nextSong = album.songs[nextSongIndex];
      player.playPause(nextSong);
    });
+
+   $('#time-control input').on('input', function (event) {
+       player.skipTo(event.target.value);
+  });
+
+   setInterval( () => {
+        if (player.playState !== 'playing') { return; }
+        const currentTime = player.getTime();
+  const duration = player.getDuration();
+  const percent = (currentTime / duration) * 100;
+      $('#time-control .current-time').text( currentTime );
+       $('#time-control input').val(percent);
+  }, 1000);
 }
